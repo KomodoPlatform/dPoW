a4d886da2d573785879ae2a7cc7e8dfe89adfb98
phm87 <31578435+phm87@users.noreply.github.com>
2020-09-18 00:00:05 +0200

    Update dpow_rpc.c

--- iguana/dpow/dpow_rpc.c
+++ iguana/dpow/dpow_rpc.c
@@ -1020,7 +1020,7 @@ int32_t dpow_vini_ismine(struct supernet_info *myinfo,struct dpow_info *dp,cJSON
 
 int32_t dpow_haveutxo(struct supernet_info *myinfo,struct iguana_info *coin,bits256 *txidp,int32_t *voutp,char *coinaddr,char *srccoin)
 {
-    int32_t vout,haveutxo = 0; uint32_t i,j,n,r,l; bits256 txid; cJSON *unspents,*item; uint64_t satoshis; char *str,*address; uint8_t script[35];
+    int32_t vout,haveutxo = 0; uint32_t i,j,n,r; bits256 txid; cJSON *unspents,*item; uint64_t satoshis; char *str,*address; uint8_t script[35];
     if ( coin->active == 0 ) return (0);
     memset(txidp,0,sizeof(*txidp));
     *voutp = -1;
