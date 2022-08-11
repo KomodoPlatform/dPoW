23792ce5f3ea0b4e5898eed146aef19ac0b4a679
ComputerGenie <tim@yourcomputergenie.com>
2022-08-11 07:26:50 -0500

    A little conditioner
    A little less screen clutter

--- iguana/dpow/dpow_network.c
+++ iguana/dpow/dpow_network.c
@@ -2127,7 +2127,7 @@ void dpow_notarize_update(struct supernet_info *myinfo,struct dpow_info *dp,stru
                 printf("mypaxcrc.%x\n",bp->paxwdcrc);
         }
         char str[65];
-        if ( (rand() % 130) == 0 ) {
+        if ( (rand() % 130) == 0 && matches >10 && bp->bestk > 0) {
             snprintf(colour, sizeof(colour), strcmp(dp->symbol, "KMD") == 0 ? GREEN : YELLOW);
             printf("%s[%s] "RESET,colour,dp->symbol);
             printf("State: NOTARIZE paxwdcrc.%x ht.%d minsig.%d peers.%d matches.%d paxmatches.%d bestmatches.%d bestk.%d:%d %llx recv.%llx sigmasks.(%llx %llx) senderid.%d myid.%d state.%x (%x %x %x) MoM.%s [%d]\n"RESET,bp->paxwdcrc,bp->height,bp->minsigs,dp->numipbits,matches,paxmatches,bestmatches,bp->bestk,bp->pendingbestk,(long long)bp->bestmask,(long long)bp->recvmask,(long long)(bp->bestk>=0?bp->destsigsmasks[bp->bestk]:0),(long long)(bp->bestk>=0?bp->srcsigsmasks[bp->bestk]:0),senderind,bp->myind,bp->state,bp->hashmsg.uints[0],bp->desttxid.uints[0],bp->srctxid.uints[0],bits256_str(str,bp->MoM),bp->MoMdepth);
